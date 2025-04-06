228b45e19c25a110ed8de926bf7e302714e2af1a
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-04-06 14:20:03 +0100

    feat(controller): add swagger documentation

--- api/src/main/java/com/example/autoship/controllers/RepositoryController.java
+++ api/src/main/java/com/example/autoship/controllers/RepositoryController.java
@@ -2,6 +2,7 @@ package com.example.autoship.controllers;
 
 import com.example.autoship.services.OauthService;
 import com.example.autoship.utils.ResponseBuilder;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +19,14 @@ public class RepositoryController {
     private final OauthService oauthService;
     private final ResponseBuilder responseBuilder;
 
+    @Operation(summary = "Get Repos", responses = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Successfully Fetching Repos for user ."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error while trying to send http request to github api")
+    })
     @GetMapping
     public ResponseEntity<?> getUserRepos(@RequestHeader("Authorization") String authHeader){
         // 1. Extract JWT
-        String jwtToken = authHeader.substring(7); // Remove "Bearer " prefix
+        String jwtToken = authHeader.substring(7);
         return responseBuilder.buildResponse(HttpStatus.OK.value(),"User repos fetched successfully",oauthService.getUserRepos(jwtToken));
     }
 }
