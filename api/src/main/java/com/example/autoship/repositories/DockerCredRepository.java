afb433d97a93c49c353516fcf3e95d5d4259eacf
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-04-15 17:36:00 +0100

    refactor(repository): adjust queries and interfaces for new entity structure

--- api/src/main/java/com/example/autoship/repositories/DockerCredRepository.java
+++ api/src/main/java/com/example/autoship/repositories/DockerCredRepository.java
@@ -2,6 +2,11 @@ package com.example.autoship.repositories;
 
 import com.example.autoship.models.DockerCredentials;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DockerCredRepository extends JpaRepository<DockerCredentials,Long> {
+
 }
--- api/src/main/java/com/example/autoship/repositories/ProjectRepository.java
+++ api/src/main/java/com/example/autoship/repositories/ProjectRepository.java
@@ -1,7 +1,11 @@
 package com.example.autoship.repositories;
 
+import com.example.autoship.models.DockerCredentials;
 import com.example.autoship.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+
 }
