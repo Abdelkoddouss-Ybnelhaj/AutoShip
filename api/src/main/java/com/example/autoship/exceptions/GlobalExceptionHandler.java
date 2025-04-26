8bed8e07c1caff8ab9d5cfa785c368b8e11a5ed6
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-04-26 12:04:08 +0100

    fix(exception): handle new exceptions in global exception handler

--- api/src/main/java/com/example/autoship/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/com/example/autoship/exceptions/GlobalExceptionHandler.java
@@ -7,8 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.autoship.common.MessageKeys.*;
 
@@ -27,17 +31,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(GithubRequestException.class)
     public ResponseEntity<ApiResponse<String>> handleGithubRequestException(GithubRequestException ex) {
-        return responseBuilder.buildResponse(ex.getStatus(), ex.getStatus() == 404 ?  ERROR_REPO_NOT_FOUND : ERROR_HOOK_EXIST);
+        return responseBuilder.buildResponse(ex.getStatus(), ex.getStatus() == 404 ?  ERROR_RESOURCE_NOT_FOUND : ERROR_HOOK_EXIST);
     }
 
     @ExceptionHandler(InvalidJwtException.class)
-    public ResponseEntity<ApiResponse<Object>> handleInvalidJwtException(InvalidJwtException ex) {
+    public ResponseEntity<ApiResponse<String>> handleInvalidJwtException(InvalidJwtException ex) {
         log.warn("Invalid JWT: {}", ex.getMessage());
         return responseBuilder.buildResponse(401,ex.getMessage());
     }
 
+    @ExceptionHandler(DeploymentNotFoundException.class)
+    public ResponseEntity<ApiResponse<String>> handleDeploymentNotFoundException(DeploymentNotFoundException ex) {
+        log.warn(ex.getMessage());
+        return responseBuilder.buildResponse(404,ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return responseBuilder.buildResponse(HttpStatus.BAD_REQUEST.value(),"Validation Error",errors);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleGlobalException(Exception ex) {
+        log.warn(ex.getMessage());
         return responseBuilder.buildResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ERROR_UNEXPECTED_ERROR);
     }
 }
